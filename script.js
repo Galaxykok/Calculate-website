f04ee31328eea0f98a13a5c00c9748d1e7a980a1
FELIPE HORACIO MATEU <u23131@academico.cotuca.unicamp.br>
2023-08-24 08:50:39 -0300

    Update string

---
+++ script.js
@@ -0,0 +1,23 @@
+let res = document.querySelector("div#result");
+
+function fac() {
+    let value = document.querySelector("#factor").value;
+    let fac = 1;
+    for (let a = 1; a <= value; a++) {
+        fac *= a;
+    }
+    return fac;
+}
+function calc() {
+    let value = parseFloat(document.querySelector("#factor").value);
+    if (Number.isNaN(value)) return;
+    if (value % 1 !== 0) {
+        res.innerHTML = "There is no factorial of decimals";
+        return;
+    }
+    if (value < 0) {
+        res.innerHTML = "There is no factorial of negatives";
+        return;
+    }
+    res.innerHTML = `The factorial of ${value} is ${fac()}`;
+}
